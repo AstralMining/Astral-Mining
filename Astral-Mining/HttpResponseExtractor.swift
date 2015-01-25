dd96ad323319396634f08eb2ece1a4f03207c8de
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-01-25 14:06:22 +0100

    Improved unit tests for the HttpResponseExtractor.

--- Astral-Mining/HttpResponseExtractor.swift
+++ Astral-Mining/HttpResponseExtractor.swift
@@ -13,6 +13,20 @@ class HttpResponseExtractor {
     }
     
     func sessionCookie() -> String? {
-        return nil
+        if let header = response.allHeaderFields["Set-Cookie"] as? NSString {
+            let cookies = header.componentsSeparatedByCharactersInSet(
+                NSCharacterSet(charactersInString: ";")) as [NSString]
+            
+            for cookie in cookies {
+                let trimmedCookie = cookie.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) as NSString
+                if trimmedCookie.hasPrefix("session=") {
+                    return trimmedCookie as String
+                }
+            }
+            
+            return nil
+        } else {
+            return nil
+        }
     }
 }
--- Astral-MiningTests/HttpResponseExtractorTests.swift
+++ Astral-MiningTests/HttpResponseExtractorTests.swift
@@ -14,18 +14,34 @@ class HttpResponseExtractorTests: XCTestCase {
     }
 
     func testCorrectStatusCodeShallBeExtracted() {
-        let response = NSHTTPURLResponse(URL: NSURL(string: "/anything")!, statusCode: 200,
-            HTTPVersion: "1.1", headerFields: nil)!
-        let extractor = HttpResponseExtractor(response: response)
+        let extractor = mkHttpResponseExtractor(nil)
         
         XCTAssertEqual(200, extractor.statusCode(), "Status codes shall be equal")
     }
     
     func testExtractSessionCookieForEmptyHeaders() {
-        let response = NSHTTPURLResponse(URL: NSURL(string: "/anything")!, statusCode: 200,
-            HTTPVersion: "1.1", headerFields: nil)!
-        let extractor = HttpResponseExtractor(response: response)
+       let extractor = mkHttpResponseExtractor(nil)
         
         XCTAssertNil(extractor.sessionCookie(), "Shall be nil session cookie")
+    }
+    
+    func testExtractSessionCookieFromHeaders() {
+        var headers = Dictionary<String, String>()
+        headers["Set-Cookie"] = "foo=bar; session=123; baz=baaz"
+        let extractor = mkHttpResponseExtractor(headers)
+
+        if let sessionCookie = extractor.sessionCookie() {
+            XCTAssertEqual("session=123", sessionCookie, "Shall be equal cookies")
+        } else {
+            XCTAssertTrue(false, "Shall not be nil")
+        }
+    }
+    
+    private func mkHttpResponseExtractor(headerFields: [String: String]?) -> HttpResponseExtractor {
+        let response = NSHTTPURLResponse(URL: NSURL(string: "/anything")!, statusCode: 200,
+                HTTPVersion: "HTTP/1.1", headerFields: headerFields)!
+        let extractor = HttpResponseExtractor(response: response)
+        
+        return extractor
     }
 }
